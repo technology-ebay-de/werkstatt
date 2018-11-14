bf1ae9a32e7bbb4e6fc2b519bedad71f67b64605
Jan Ehrhardt <janehrhardt@ebay.com>
2018-11-14 07:00:01 +0100

    Use modular-toolkit
    modular-toolkit is based on a useful structure, that will serve as the
    foundation of werkstatt's UI. The set up has been done by copying the
    example app from the modular-toolkit. Thus there might be some code,
    that has not been removed from the example.

---
+++ web/src/components/ScratchPad/ScratchPad.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './scratch-pad.css';
+
+const ScratchPad = ({ changeColor }) => (
+    <main className="scratch-pad">
+    </main>
+);
+
+export default ScratchPad;
---
+++ web/src/components/ScratchPad/ScratchPadContainer.js
@@ -0,0 +1,10 @@
+import { compose, setDisplayName } from 'recompose';
+import { connect } from 'react-redux';
+import ScratchPad from './ScratchPad';
+
+const enhance = compose(
+    setDisplayName('ScratchPadContainer'),
+    connect()
+);
+
+export default enhance(ScratchPad);
