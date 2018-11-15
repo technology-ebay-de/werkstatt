f57750166c4ec6aaa96a404ec7172ba313565539
Jan Ehrhardt <janehrhardt@ebay.com>
2018-11-15 03:50:18 +0100

    Use neutrino instead of react-scripts
    neutrino works smoother and does out of the box what it should do.

---
+++ web/.neutrinorc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    use: [
+        ['@neutrinojs/eslint',
+            {
+                eslint: {
+                    rules: {
+                        quotes: [
+                            "error",
+                            "single",
+                            {
+                                "avoidEscape": true
+                            }
+                        ],
+                        semi: [
+                            "error",
+                            "never"
+                        ]
+                    }
+                }
+            }
+        ],
+        [
+            '@neutrinojs/react',
+            {
+                html: {
+                    title: 'werkstatt'
+                }
+            }
+        ],
+        '@neutrinojs/jest'
+    ]
+}
