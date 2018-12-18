8dbfe5ee4bacab6bd6a10c2a9318651ab446602e
Jan Ehrhardt <janehrhardt@ebay.com>
2018-12-19 00:20:44 +0100

    Serve UI from Flask app
    For production environments the UI will be build and then served from
    the Flask based server.

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, static_url_path='', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
--- server/sketch_pad.py
+++ server/sketch_pad.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, send_from_directory
 
 
 bp = Blueprint('sketch_pad', __name__, url_prefix='/sketchpad')
 
-@bp.route("/")
-def hello():
-    return "Hello Werkstatt!"
+@bp.route('/', defaults={'path': ''})
+@bp.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists('static' + path):
+        return send_from_directory('static', path)
+    else:
+        return send_from_directory('static', 'index.html')
