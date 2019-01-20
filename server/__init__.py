3ca05d9ca44cfba1b250d871b5840546e9a884cf
Jan Ehrhardt <janehrhardt@ebay.com>
2019-01-20 22:14:11 +0100

    Build as Docker image
    Werkstatt will be released as a Docker image. This is requiered to run it on
    Kubernetes.

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, static_url_path='')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://werkstatt:werkstatt@localhost/werkstatt'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
